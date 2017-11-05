1306dfb1cf040cc3d186926c4bd4e301ee31dac7
rcyogesh <rcyogesh@gmail.com>
2017-11-05 08:27:14 -0500

    - router for BP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AtoZComponent } from './atoz.component'
@@ -18,7 +19,14 @@ import { BPComponent } from './bp.component'
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: 'BP',
+        component: BPComponent
+      }
+    ]
+      )
   ],
   providers: [],
   bootstrap: [AppComponent]

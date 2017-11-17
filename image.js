d16b8d9f798f07c3a4b90a1b0431274734360256
rcyogesh <rcyogesh@gmail.com>
2017-11-16 23:37:48 -0500

    - image cache implementation

--- image.js
+++ image.js
@@ -2,30 +2,42 @@ const fs = require('fs');
 var https = require('https');
 const util = require('util');
 
-function handleGoogleResponse(gres, response) {
-    //console.log(gres);
+function handleGoogleResponse(searchTerm, gres, response) {
         var wholeData = '';
         gres.on('data', (data)=> {
-            console.log(data);
             wholeData += data;
         });
 
 
         gres.on('end', ()=>{
-            //console.log(wholeData.toString());
             let json = wholeData.toString();
             let obj = JSON.parse(json);
-           // response.write(json);
-           let imgUrl = obj.items[0].pagemap.cse_thumbnail[0].src;
-            response.write(JSON.stringify(imgUrl));
-    response.end();
+            let arr = [];
+            obj.items.forEach(i=>i.pagemap.cse_thumbnail.forEach(t=>arr.push(t.src)));
+            cache[searchTerm] = arr;
+            returnResponse(arr, response);
         })
+}
+
+let cache = {};
+
+function returnResponse(arr, response) {
+    let index = Date.now() % arr.length;
+    response.write(JSON.stringify(arr[index]));
+    response.end();
 }
 
 module.exports = {
     process: function(term, response) {
+        let arr = cache[term];
+        if(arr == undefined) {
+            console.log("Will query");
         let url = util.format("https://www.googleapis.com/customsearch/v1?key=%s&cx=%s&q=%s&imgSize=medium",
             process.env.googlekey, process.env.googlecx, term);
-        https.get(url, gres => handleGoogleResponse(gres, response));
+        https.get(url, gres => handleGoogleResponse(term, gres, response));
+        }
+        else {
+            returnResponse(arr, response);
+        }
     }
 }
